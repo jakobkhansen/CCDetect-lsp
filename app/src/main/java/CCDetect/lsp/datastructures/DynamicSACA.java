7865f01d80e50255b90db5b92f25213ea96eaf34
Jakob Hansen <jakob.hansen@hotmail.no>
2022-11-29 18:58:54 +0100

    Intermediate commit with no progress :)

--- app/src/main/java/CCDetect/lsp/datastructures/DynamicSACA.java
+++ app/src/main/java/CCDetect/lsp/datastructures/DynamicSACA.java
@@ -1,10 +1,5 @@
 package CCDetect.lsp.datastructures;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.SortedMap;
-import java.util.TreeMap;
 import java.util.logging.Logger;
 
 import CCDetect.lsp.utils.Printer;
@@ -17,15 +12,16 @@ public class DynamicSACA {
             Logger.GLOBAL_LOGGER_NAME);
 
     public ExtendedSuffixArray insertSingleChar(ExtendedSuffixArray suff, int[] oldText, int[] newText, int position) {
-        LOGGER.info("oldText: " + Printer.print(oldText));
-        LOGGER.info("newText: " + Printer.print(newText));
-        LOGGER.info("oldSuffix: " + Printer.print(suff.getSuffix()));
-        LOGGER.info("oldISA: " + Printer.print(suff.getInverseSuffix()));
+        System.out.println("oldText: " + Printer.print(oldText));
+        System.out.println("newText: " + Printer.print(newText));
+        // LOGGER.info("oldSuffix: " + Printer.print(suff.getSuffix()));
+        // LOGGER.info("oldISA: " + Printer.print(suff.getInverseSuffix()));
         int[] oldSA = suff.getSuffix();
         int[] oldISA = suff.getInverseSuffix();
         int[] newSA = new int[oldSA.length + 1];
         int[] newISA = new int[oldISA.length + 1];
         int[] l = getL(suff, oldText);
+        System.out.println("l: " + Printer.print(l));
 
         // Stage 1, copy elements which are not changed
         for (int i = 0; i < oldSA.length; i++) {
@@ -33,70 +29,61 @@ public class DynamicSACA {
             newISA[newSA[i]] = i;
         }
 
+        int originalPos = getLFDynamic(oldISA[position], l, oldText);
+
         // Stage 2, update L
         int storedLetter = l[oldISA[position]];
         l[oldISA[position]] = newText[position];
 
         // Stage 3, insert new row, increasing SA at all values larger than the location
         // its inserted
-        int pos = getLFDynamic(newISA[position], oldSA, l, oldText);
-
-        System.out.println("here " + storedLetter);
-        System.out.println(Printer.print(l));
-        System.out.println(pos);
 
         // Insert new row in L
-        for (int i = l.length - 2; i >= pos; i--) {
-            l[i + 1] = l[i];
-        }
-        l[pos] = storedLetter;
+        insert(l, originalPos, storedLetter);
 
         // Increment all values in SA greater than or equal to position
         for (int i = position; i < oldISA.length; i++) {
             newSA[oldISA[i]]++;
         }
+        int pos = originalPos + (originalPos >= position ? 1 : 0);
+        // int pos = getLFDynamic(oldISA[position], l, newText);
 
         // Increment all values in ISA greater than or equal to LF(ISA[position])
-        for (int i = pos; i < oldSA.length; i++) {
+        for (int i = originalPos; i < oldSA.length; i++) {
             newISA[oldSA[i]]++;
         }
 
         // Insert new row in SA
-        for (int i = newSA.length - 2; i >= pos; i--) {
-            newSA[i + 1] = newSA[i];
-        }
-        newSA[pos] = position;
+        insert(newSA, originalPos, position);
 
         // Insert new row in ISA
-        for (int i = newISA.length - 2; i >= newSA[pos]; i--) {
-            newISA[i + 1] = newISA[i];
-        }
-        newISA[newSA[pos]] = pos;
+        insert(newISA, newSA[originalPos], originalPos);
 
-        if (pos >= position) {
-            System.out.println("Incremented");
-            pos++;
-        }
-        System.out.println("Suff before stage 4 " + Printer.print(newSA));
+        System.out.println("L before stage 4 " + Printer.print(l));
+        System.out.println("SA before stage 4 " + Printer.print(newSA));
+        System.out.println("ISA before stage 4 " + Printer.print(newISA));
         System.out.println("pos " + pos);
-
         // Stage 4
-        int posLF = getLFDynamic(pos - 1, newSA, l, newText);
-        System.out.println("posLF " + posLF);
-        System.exit(0);
+
+        // I think the problem is that posLF is not calculated correctly, according to
+        // Reorder function in dyn suff paper
+        // Look at how LF(index(T'[i])) is calculated
+        int posLF = getLFDynamic((pos - 1), l, newText);
         while (pos != posLF) {
-            System.out.println(pos);
-            System.out.println(posLF);
-            int newPos = getLFDynamic(pos, newSA, l, newText);
+            int newPos = getLFDynamic(pos, l, newText);
             moveRow(pos, posLF, newSA, newISA, l);
             pos = newPos;
-            posLF = getLFDynamic(posLF, newSA, l, newText);
+            posLF = getLFDynamic(posLF, l, newText);
+            System.out.println("pos " + pos);
+            System.out.println("posLF " + posLF);
         }
+        System.out.println("final SA " + Printer.print(newSA));
 
         return new ExtendedSuffixArray(newSA, newISA, new SAIS().buildLCPArray(newText, newSA, newISA));
     }
 
-    private int[] getL(ExtendedSuffixArray suff, int[] oldText) {
+    // Returns L with an extra spot for added character
+    public int[] getL(ExtendedSuffixArray suff, int[] oldText) {
         int[] l = new int[suff.getSuffix().length + 1];
 
         for (int i = 0; i < l.length - 1; i++) {
@@ -105,13 +92,12 @@ public class DynamicSACA {
         return l;
     }
 
-    private int getLFDynamic(int index, int[] sa, int[] l, int[] text) {
+    public int getLFDynamic(int index, int[] l, int[] text) {
 
         int charsBefore = 0;
         for (int i = 0; i < text.length; i++) {
             charsBefore += text[i] < l[index] ? 1 : 0;
         }
-
         int rank = 0;
         for (int i = 0; i < index; i++) {
             rank += l[i] == l[index] ? 1 : 0;
@@ -126,7 +112,7 @@ public class DynamicSACA {
             j = i;
             i = tmp;
         }
-        System.out.println("SA Setting " + i + " to " + j);
+        // System.out.println("SA Setting " + i + " to " + j);
         // Update L
         int tmp = l[i];
         l[i] = l[j];
@@ -140,10 +126,17 @@ public class DynamicSACA {
 
         // Update ISA
         for (int index = i; index < j; index++) {
-            System.out.println("ISA Decrementing " + newSA[index]);
+            // System.out.println("ISA Decrementing " + newSA[index]);
             newISA[newSA[index]]--;
         }
-        System.out.println("setting " + iPos + " to " + j);
+        // System.out.println("setting " + iPos + " to " + j);
         newISA[iPos] = j;
+    }
+
+    private void insert(int[] arr, int index, int element) {
+        for (int i = arr.length - 1; i > index; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[index] = element;
     }
 }
--- app/src/test/java/CCDetect/lsp/DynamicSACATest.java
+++ app/src/test/java/CCDetect/lsp/DynamicSACATest.java
@@ -1,11 +1,11 @@
 package CCDetect.lsp;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.logging.Logger;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import org.junit.Test;
 
@@ -19,28 +19,72 @@ import CCDetect.lsp.utils.Printer;
  */
 public class DynamicSACATest {
 
+    private static final Logger LOGGER = Logger.getLogger(
+            Logger.GLOBAL_LOGGER_NAME);
+
     @Test
-    public void testUpdateSingleCharacter() {
+    public void testLF() {
+        String input = "pneumonoultramicroscopicsilicovolcanoconiosis";
+        int[] inputArr = stringToIntArray(input);
+        SAIS sais = new SAIS();
+        ExtendedSuffixArray suff = sais.buildExtendedSuffixArray(inputArr);
+
+        DynamicSACA saca = new DynamicSACA();
+        int[] l = saca.getL(suff, inputArr);
+
+        int current = 0;
+        boolean found = false;
+        for (int i = 0; i < inputArr.length; i++) {
+            if (saca.getLFDynamic(i, l, inputArr) == 0) {
+                current = i;
+                found = true;
+            }
+        }
+        assertTrue(found);
+        for (int i = inputArr.length - 1; i >= 0; i--) {
+            assertEquals(inputArr[i], l[current]);
+            current = saca.getLFDynamic(current, l, inputArr);
+        }
+    }
+
+    public void testDynamicSuffix(String input, String edit, int position) {
         SAIS sais = new SAIS();
         DynamicSACA dynSACA = new DynamicSACA();
-        String originalText = "qweroiupxzcvpoiuqweroiu";
-        System.out.println("originalText: " + originalText);
-        int[] original = stringToIntArray(originalText);
+        int[] original = stringToIntArray(input);
         ExtendedSuffixArray eSuffBanana = sais.buildExtendedSuffixArray(original);
-        int[] updated = stringToIntArray("qweroxiupxzcvpoiuqweroiu");
-        ExtendedSuffixArray eSuffUpdated = dynSACA.insertSingleChar(eSuffBanana, original, updated, 5);
+        int[] updated = stringToIntArray(edit);
 
         ExtendedSuffixArray expected = sais.buildExtendedSuffixArray(updated);
 
+        ExtendedSuffixArray eSuffUpdated = dynSACA.insertSingleChar(eSuffBanana,
+                original, updated, position);
+
         System.out.println("Expected SA: " + Printer.print(expected.getSuffix()));
         System.out.println("Actual SA: " + Printer.print(eSuffUpdated.getSuffix()));
-        // assertArrayEquals(expected.getSuffix(), eSuffUpdated.getSuffix());
+        assertArrayEquals(expected.getSuffix(), eSuffUpdated.getSuffix());
 
-        System.out.println("Expected ISA: " + Printer.print(expected.getInverseSuffix()));
-        System.out.println("Actual ISA: " + Printer.print(eSuffUpdated.getInverseSuffix()));
+        System.out.println("Expected ISA: " +
+                Printer.print(expected.getInverseSuffix()));
+        System.out.println("Actual ISA: " +
+                Printer.print(eSuffUpdated.getInverseSuffix()));
 
         assertArrayEquals(expected.getInverseSuffix(),
                 eSuffUpdated.getInverseSuffix());
+
+    }
+
+    @Test
+    public void testInsertSingleCharacter() {
+        testDynamicSuffix("ctctgc", "ctgctgc", 2);
+    }
+
+    @Test
+    public void testShortSingleInsert() {
+        testDynamicSuffix("ab", "aab", 0);
+        testDynamicSuffix("ab", "aab", 1);
+
+        testDynamicSuffix("ab", "abb", 1);
+        // testDynamicSuffix("ab", "abb", 2);
     }
 
     public int[] stringToIntArray(String input) {
@@ -48,4 +92,5 @@ public class DynamicSACATest {
             return (int) c - ('a' - 1);
         }), IntStream.of(0)).toArray();
     }
+
 }
