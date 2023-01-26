b340e346dfda57aaf457560642a8575164d651ca
Jakob Hansen <jakob.hansen@hotmail.no>
2023-01-26 16:26:57 +0100

    Better charCount

---
+++ app/src/main/java/CCDetect/lsp/datastructures/CharacterCount.java
@@ -0,0 +1,37 @@
+package CCDetect.lsp.datastructures;
+
+import CCDetect.lsp.utils.Printer;
+
+public class CharacterCount {
+    int[] charCounts;
+    int realSize;
+
+    public CharacterCount(int[] chars) {
+        realSize = chars.length;
+        int max = 0;
+        for (int ch : chars) {
+            max = ch > max ? ch : max;
+        }
+        charCounts = new int[max + 200];
+
+        for (int ch : chars) {
+            charCounts[ch]++;
+        }
+    }
+
+    public int getNumberOfSmallerChars(int ch) {
+        int sum = 0;
+        for (int i = ch - 1; i >= 0; i--) {
+            sum += charCounts[i];
+        }
+        return sum;
+    }
+
+    public void deleteChar(int ch) {
+        charCounts[ch]--;
+    }
+
+    public void addChar(int ch) {
+        charCounts[ch]++;
+    }
+}
