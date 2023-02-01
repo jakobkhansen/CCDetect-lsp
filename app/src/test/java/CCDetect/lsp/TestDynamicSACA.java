d73bfb6bf5be09d8d162bf129d4c37bd58dd43bd
Jakob Hansen <jakob.hansen@hotmail.no>
2023-02-01 11:27:24 +0100

    Fix small bug that made a single LCP value not update in some cases

--- app/src/test/java/CCDetect/lsp/TestDynamicSACA.java
+++ app/src/test/java/CCDetect/lsp/TestDynamicSACA.java
@@ -4,7 +4,10 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+import java.util.UUID;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.junit.Test;
@@ -28,79 +31,80 @@ public class TestDynamicSACA {
 
     @Test
     public void testShortSingleInsert() {
-        // testDynamicSuffixInsertFactor("cacgacg", "a", 3);
-        // testDynamicSuffixInsertFactor("ab", "a", 0);
-        // testDynamicSuffixInsertFactor("ab", "a", 1);
-        // testDynamicSuffixInsertFactor("dc", "b", 1);
-        // testDynamicSuffixInsertFactor("ab", "x", 1);
-        // testDynamicSuffixInsertFactor("abc", "x", 1);
-        // testDynamicSuffixInsertFactor("axbc", "d", 4);
-        // testDynamicSuffixInsertFactor("axbcd", "a", 5);
-        //
-        // testDynamicSuffixInsertFactor("mississippi", "i", 4);
-        // testDynamicSuffixInsertFactor("mississippi", "i", 5);
-        //
-        // testDynamicSuffixInsertFactor("pneumonoultramicroscopicsilicovolcanoconiosis",
-        // "x", 10);
+        testDynamicSuffixInsertFactor("cacgacg", "a", 3);
+        testDynamicSuffixInsertFactor("ab", "a", 0);
+        testDynamicSuffixInsertFactor("ab", "a", 1);
+        testDynamicSuffixInsertFactor("dc", "b", 1);
+        testDynamicSuffixInsertFactor("ab", "x", 1);
+        testDynamicSuffixInsertFactor("abc", "x", 1);
+        testDynamicSuffixInsertFactor("axbc", "d", 4);
+        testDynamicSuffixInsertFactor("axbcd", "a", 5);
+
+        testDynamicSuffixInsertFactor("mississippi", "i", 4);
+        testDynamicSuffixInsertFactor("mississippi", "i", 5);
+
+        testDynamicSuffixInsertFactor("pneumonoultramicroscopicsilicovolcanoconiosis",
+                "x", 10);
     }
 
     @Test
     public void testInsertSingleCharOnAllIndices() {
-        // testInsertOnAllIndices("helloworld", "a");
-        // testInsertOnAllIndices("pneumonoultramicroscopicsilicovolcanoconiosis", "a");
-        // testInsertOnAllIndices("pneumonoultramicroscopicsilicovolcanoconiosis", "x");
-        // testInsertOnAllIndices("pneumonoultramicroscopicsilicovolcanoconiosis", "h");
+        testInsertOnAllIndices("helloworld", "a");
+        testInsertOnAllIndices("pneumonoultramicroscopicsilicovolcanoconiosis", "a");
+        testInsertOnAllIndices("pneumonoultramicroscopicsilicovolcanoconiosis", "x");
+        testInsertOnAllIndices("pneumonoultramicroscopicsilicovolcanoconiosis", "h");
     }
 
     @Test
     public void testInsertSmallFactor() {
-        // testDynamicSuffixInsertFactor("b", "a", 0);
-        // testDynamicSuffixInsertFactor("ctctgc", "g", 2);
-        // testDynamicSuffixInsertFactor("b", "ac", 1);
-        // testDynamicSuffixInsertFactor("b", "acd", 1);
-        // testDynamicSuffixInsertFactor("b", "a", 0);
-        // testDynamicSuffixInsertFactor("b", "ab", 0);
-        // testDynamicSuffixInsertFactor("ac", "da", 1);
-        // testDynamicSuffixInsertFactor("ab", "ab", 2);
-        // testDynamicSuffixInsertFactor("b", "bb", 0);
-        // testDynamicSuffixInsertFactor("b", "asldkjsalkdj", 0);
-        // testDynamicSuffixInsertFactor("bcd", "bcad", 1);
-        // testDynamicSuffixInsertFactor("bcd", "xxx", 3);
-        // testDynamicSuffixInsertFactor("bcd", "xadpx", 1);
-        // testDynamicSuffixInsertFactor("bcd", "cd", 2);
-        // testInsertOnAllIndices("bcd", "hx");
-        // testInsertOnAllIndices("bcd", "aj");
+        testDynamicSuffixInsertFactor("b", "a", 0);
+        testDynamicSuffixInsertFactor("ctctgc", "g", 2);
+        testDynamicSuffixInsertFactor("b", "ac", 1);
+        testDynamicSuffixInsertFactor("b", "acd", 1);
+        testDynamicSuffixInsertFactor("b", "a", 0);
+        testDynamicSuffixInsertFactor("b", "ab", 0);
+        testDynamicSuffixInsertFactor("ac", "da", 1);
+        testDynamicSuffixInsertFactor("ab", "ab", 2);
+        testDynamicSuffixInsertFactor("b", "bb", 0);
+        testDynamicSuffixInsertFactor("b", "asldkjsalkdj", 0);
+        testDynamicSuffixInsertFactor("bcd", "bcad", 1);
+        testDynamicSuffixInsertFactor("bcd", "xxx", 3);
+        testDynamicSuffixInsertFactor("bcd", "xadpx", 1);
+        testDynamicSuffixInsertFactor("bcd", "cd", 2);
+        testInsertOnAllIndices("bcd", "hx");
+        testInsertOnAllIndices("bcd", "aj");
     }
 
     @Test
     public void testInsertFactorOnAllIndices() {
-        // testInsertOnAllIndices("pneumonoultramicroscopicsilicovolcanoconiosis",
-        // "habc");
-        // testInsertOnAllIndices("pneumonoultramicroscopicsilicovolcanoconiosis",
-        // "xxxxxasldkjsadoiqw");
+        testInsertOnAllIndices("pneumonoultramicroscopicsilicovolcanoconiosis",
+                "habc");
+        testInsertOnAllIndices("pneumonoultramicroscopicsilicovolcanoconiosis",
+                "xxxxxasldkjsadoiqw");
     }
 
     @Test
     public void deleteSmallFactor() {
-        // testDynamicSuffixDeleteFactor("abc", 1, 2);
-        // testDynamicSuffixDeleteFactor("abcd", 1, 2);
-        // testDynamicSuffixDeleteFactor("abcde", 1, 2);
-        // testDynamicSuffixDeleteFactor("abcde", 1, 3);
-        // testDynamicSuffixDeleteFactor("abcde", 1, 4);
-        // testDynamicSuffixDeleteFactor("ba", 1, 1);
-        // testDynamicSuffixDeleteFactor("jsaldkj", 0, 4);
-        // testDynamicSuffixDeleteFactor("abb", 2, 1);
+        testDynamicSuffixDeleteFactor("abc", 1, 2);
+        testDynamicSuffixDeleteFactor("abcd", 1, 2);
+        testDynamicSuffixDeleteFactor("abcde", 1, 2);
+        testDynamicSuffixDeleteFactor("abcde", 1, 3);
+        testDynamicSuffixDeleteFactor("abcde", 1, 4);
+        testDynamicSuffixDeleteFactor("ba", 1, 1);
+        testDynamicSuffixDeleteFactor("jsaldkj", 0, 4);
+        testDynamicSuffixDeleteFactor("abb", 2, 1);
 
     }
 
     @Test
     public void deleteAllFactorsInString() {
-        // testDeleteAllFactors("abc");
-        // testDeleteAllFactors("abb");
-        // testDeleteAllFactors("pneumonoultramicroscopicsilicovolcanoconiosis");
-        // testDeleteAllFactors("floccinaucinihilipilification");
-        // testDeleteAllFactors("incomprehensibility");
-        // testDeleteAllFactors("xenotransplantation");
+        testDeleteAllFactors("abc");
+        testDeleteAllFactors("abb");
+        testDeleteAllFactors("pneumonoultramicroscopicsilicovolcanoconiosis");
+        testDeleteAllFactors("floccinaucinihilipilification");
+        testDeleteAllFactors("simplification");
+        testDeleteAllFactors("incomprehensibility");
+        testDeleteAllFactors("xenotransplantation");
     }
 
     @Test
@@ -144,6 +148,28 @@ public class TestDynamicSACA {
     @Test
     public void testLCPDelete() {
         testLCPDeleteOnAllIndices("pneumonoultramicroscopicsilicovolcanoconiosis");
+        testDynamicSuffixDeleteFactor("baba", 1, 1);
+    }
+
+    @Test
+    public void testRandomString() {
+        // for (int i = 0; i < 1000; i++) {
+        //
+        // UUID randomUUID = UUID.randomUUID();
+        //
+        // String str = randomUUID.toString().replaceAll("-", "").replaceAll("\\d", "");
+        // System.out.println("uuid: " + str);
+        // char[] withChars = str.chars().map((int num) -> {
+        // System.out.println(num);
+        // return (Integer.valueOf((num)));
+        // }).mapToObj(j -> Character.toString((char)
+        // j)).collect(Collectors.joining()).toCharArray();
+        // System.out.println("a val: " + ((int) 'a'));
+        // String longStr = String.valueOf(withChars);
+        // String shortStr = longStr.substring(0, 4);
+        // System.out.println("Generated string: " + shortStr);
+        // testDeleteAllFactors(shortStr);
+        // }
     }
 
     public void testDynamicSuffixInsertFactor(String input, String edit, int position) {
@@ -185,8 +211,12 @@ public class TestDynamicSACA {
     public void testDynamicSuffixDeleteFactor(String input, int position, int length) {
         SAIS sais = new SAIS();
         int[] originalArray = stringToIntArrayWithTerminator(input);
-        ExtendedSuffixArray eSuffOriginal = sais.buildExtendedSuffixArray(originalArray);
         int[] resultArray = stringToIntArrayWithTerminator(getStringWithDelete(input, position, length));
+
+        System.out.println("Input array: " + Printer.print(originalArray));
+        System.out.println("Result array: " + Printer.print(resultArray));
+
+        ExtendedSuffixArray eSuffOriginal = sais.buildExtendedSuffixArray(originalArray);
 
         Timer linearTimer = new Timer();
         linearTimer.start();
@@ -194,11 +224,17 @@ public class TestDynamicSACA {
         linearTimer.stop();
 
         DynamicSACA dynSACA = new DynamicSACA(originalArray, eSuffOriginal, eSuffOriginal.size() + 100);
+        System.out.println("Initial SA: " + Printer.print(dynSACA.getPermutation().toArray()));
+        System.out.println("Initial LCP: " + Printer.print(dynSACA.getDynLCP().toArray()));
         Timer incrementalTimer = new Timer();
         incrementalTimer.start();
         dynSACA.deleteFactor(new EditOperation(EditOperationType.DELETE, position, new int[length]));
         ExtendedSuffixArray eSuffUpdated = dynSACA.getESuffFromPermutation(resultArray);
         incrementalTimer.stop();
+        System.out.println("Final SA: " + Printer.print(dynSACA.getPermutation().toArray()));
+        System.out.println("Final LCP: " + Printer.print(dynSACA.getDynLCP().toArray()));
+        System.out.println("Expected LCP: " + Printer.print(expected.getLcp()));
+        System.out.println("Expected SA: " + Printer.print(expected.getSuffix()));
 
         assertArrayEquals(expected.getSuffix(), eSuffUpdated.getSuffix());
 
@@ -211,6 +247,7 @@ public class TestDynamicSACA {
     public void testDeleteAllFactors(String input) {
         for (int i = 0; i <= input.length(); i++) {
             for (int j = 1; j <= input.length() - i; j++) {
+                System.out.println("here: " + i + " " + j);
                 testDynamicSuffixDeleteFactor(input, i, j);
             }
         }
