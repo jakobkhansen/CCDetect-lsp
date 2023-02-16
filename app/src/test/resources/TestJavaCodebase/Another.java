b6be8172c3ee2e6fc7e88b4055359a767a12b0f9
Jakob Hansen <jakob.hansen@hotmail.no>
2023-02-16 14:04:38 +0100

    Fix tests

--- app/src/test/resources/TestJavaCodebase/Another.java
+++ app/src/test/resources/TestJavaCodebase/Another.java
@@ -9,10 +9,5 @@ import org.eclipse.lsp4j.Range;
 public class Another {
     // Clone in Class.java
     public static void main(String[] args) {
-        String hello = "Hello world";
-        int x = 10;
-        double y = 10.3;
-        Range range = new Range(new Position(0, 0), new Position(1, 7));
-        System.out.println("x: " + x + ", y: " + y + ", hello: " + hello + ", range: " + range);
     }
 }
